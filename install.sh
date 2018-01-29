09fe59487941f2d6a33c7ed092521875a3e6c9a0
W. Duncan Fraser <wdf@alasconnect.com>
2018-01-29 01:19:19 -0900

    Basic i3 install

--- install.sh
+++ install.sh
@@ -2,8 +2,30 @@
 # Script for setting up Debian 9 for Duncan from a minimal install
 # So much left todo, mostly notes for now on ZSH dependencies
 
+# Enable non-free
+
 # Apt Update
 sudo apt update
+
+# Basic firmware
+sudo apt install -y \
+  firmware-linux-nonfree
+
+# Xorg and graphics
+sudo apt install -y \
+  xorg \
+  xserver-xorg \
+  nvidia-driver # TODO: Make this system generic
+
+# i3 environment
+sudo apt install -y \
+  i3 \
+  network-manager-gnome \
+  rofi \
+  scrot
+
+# Utils
+# sudo apt install -y \
 
 # Fonts
 sudo apt install -y \
@@ -19,7 +41,8 @@ sudo apt install -y \
 # Shell/ZSH Setup
 sudo apt install -y \
   zsh \
-  silversearcher-ag
+  silversearcher-ag \
+  direnv
 
 # NeoVim Setup
 sudo apt install -y \
