fcd7696313612b8d510ad46d9c2516d1d7c67d6a
W. Duncan Fraser <duncan@wduncanfraser.com>
2018-01-30 13:26:27 -0900

    More i3 env setup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# dotfiles
+# Dotfiles
 
 TBD
+
+## Credits
+
++ Pulled dunst theme from https://github.com/a-schaefers/i3-wm-gruvbox-theme
++ Gruvbox Xresources colors from https://github.com/morhetz/gruvbox-contrib
+
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Script for setting up Debian 9 for Duncan from a minimal install
 # So much left todo, mostly notes for now on ZSH dependencies
 
@@ -21,11 +21,21 @@ sudo apt install -y \
 sudo apt install -y \
   i3 \
   network-manager-gnome \
+  pulseaudio \
+  pavucontrol \
+  feh \
   rofi \
-  scrot
+  scrot \
+  rxvt-unicode-256color
 
 # Utils
-# sudo apt install -y \
+sudo apt install -y \
+  curl \
+  gawk \
+  htop \
+  wget \
+  imagemagick \
+  libnotify-bin
 
 # Fonts
 sudo apt install -y \
@@ -47,3 +57,26 @@ sudo apt install -y \
 # NeoVim Setup
 sudo apt install -y \
   neovim
+
+# Manual installs
+# Chrome
+mkdir -p ~/temp
+pushd ~/temp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# VSCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt update
+sudo apt install -y \
+	apt-transport-https \ # Needed for https repos
+	code
+
+# Slack
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.0.5-amd64.deb
+sudo apt install -y ./slack-desktop-3.0.5-amd64.deb
+
+popd
