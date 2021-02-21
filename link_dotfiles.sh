cc161b997a803105c8f9f307c51dd760a345049d
W. Duncan Fraser <duncan@relianteng.io>
2021-02-20 22:41:46 -0600

    Reworked for sway

---
+++ link_dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+ln -sfnv "${PWD}/config/dunst" "${HOME}/.config/dunst"
+ln -sfnv "${PWD}/config/fish" "${HOME}/.config/fish"
+ln -sfnv "${PWD}/config/fontconfig" "${HOME}/.config/fontconfig"
+ln -sfnv "${PWD}/config/foot" "${HOME}/.config/foot"
+ln -sfnv "${PWD}/config/gtk-3.0" "${HOME}/.config/gtk-3.0"
+ln -sfnv "${PWD}/config/rofi" "${HOME}/.config/rofi"
+ln -sfnv "${PWD}/config/sway" "${HOME}/.config/sway"
+ln -sfnv "${PWD}/config/waybar" "${HOME}/.config/waybar"
+ln -sfnv "${PWD}/.gnupg" "${HOME}/.gnupg"
