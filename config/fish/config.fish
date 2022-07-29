a1f36be4ba91b7cc5afe6d30d61a2b9762fcfdd9
W. Duncan Fraser <duncan.fraser@simspace.com>
2022-07-29 10:01:54 -0500

    Added fisher plugin manager Added pure prompt Added ~/.local/bin to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,6 +11,9 @@ set -gx PATH $HOME/.cargo/bin $PATH
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 test -f $HOME/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
 
+# local
+set -gx PATH $HOME/.local/bin $PATH
+
 # Defaults
 set -gx EDITOR nvim
 set -gx BROWSER firefox
@@ -27,3 +30,8 @@ eval (direnv hook fish)
 if status --is-interactive
     set -x GPG_TTY (tty)
 end
+
+# Prompt features
+set --universal pure_separate_prompt_on_error true
+set --universal pure_show_subsecond_command_duration true
+set --universal pure_threshold_command_duration 1
